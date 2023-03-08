9e4c1a100e3fbe2bcb3f73c72f2d705ab85822ff
foagdude <nk-again@rambler.ru>
2023-03-08 19:18:57 +0700

    Graceful shutdown

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,8 +3,13 @@
 package app
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/bbt-t/lets-go-shortener/internal/adapter/storage"
 	"github.com/bbt-t/lets-go-shortener/internal/config"
@@ -41,5 +46,19 @@ func Run(cfg config.Config) {
 	route.Post("/api/shorten/batch", handlers.URLBatch(s))
 	route.Post("/api/shorten", handlers.RecoverOriginalURLPost(s))
 
-	log.Fatal(server.ListenAndServe())
+	go func() {
+		log.Println(server.ListenAndServe())
+	}()
+	// Graceful shutdown:
+	gracefulStop := make(chan os.Signal, 1)
+	signal.Notify(gracefulStop, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	<-gracefulStop
+
+	ctxGrace, cancelGrace := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelGrace()
+	if err := server.Shutdown(ctxGrace); err != nil {
+		log.Printf("! Error shutting down server: !\n%v", err)
+	} else {
+		log.Println("! SERVER STOPPED !")
+	}
 }
