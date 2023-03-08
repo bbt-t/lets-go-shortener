c0be97305bbd780ab45306fd76a31ed35865647a
foagdude <nk-again@rambler.ru>
2023-03-08 18:04:21 +0700

    add log settings

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/bbt-t/lets-go-shortener/internal/app"
 	"github.com/bbt-t/lets-go-shortener/internal/config"
+	_ "github.com/bbt-t/lets-go-shortener/pkg/logging"
 )
 
 func main() {
