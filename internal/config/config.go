a53ed46046f24dfa7ae4b1af2e41fa9f1f83ac1c
foagdude <nk-again@rambler.ru>
2023-04-02 15:41:24 +0700

    fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -27,23 +27,9 @@ func GetDefaultConfig() Config {
 	return Config{
 		ServerAddress:   ":8080",
 		BaseURL:         "http://127.0.0.1:8080",
-		StoragePath:     "",
-		BasePath:        "",
 		DBMigrationPath: "file://migrations",
 		EnableHTTPS:     false,
 	}
-}
-
-// Singleton config creation variables.
-var (
-	cfg  = GetDefaultConfig()
-	once sync.Once
-)
-
-// GetConfig gets new config from flags or env.
-func GetConfig() Config {
-	once.Do(parseConfigs)
-	return cfg
 }
 
 // GetBenchConfig gets config for benchmarks.
@@ -53,52 +39,58 @@ func GetBenchConfig() Config {
 	}
 }
 
-// parseConfigs parse values.
-func parseConfigs() {
-	var (
-		fileCfg, envCfg, flagCfg Config
-		cfgFilePath              string
-	)
-	// flag-config
-	flag.StringVar(&flagCfg.ServerAddress, "a", cfg.ServerAddress, "Server address")
-	flag.StringVar(&flagCfg.BaseURL, "b", cfg.BaseURL, "Base OriginalURL")
-	flag.StringVar(&flagCfg.StoragePath, "f", cfg.StoragePath, "Storage path")
-	flag.StringVar(&flagCfg.BasePath, "d", cfg.BasePath, "DataBase path")
-	flag.BoolVar(&flagCfg.EnableHTTPS, "s", false, "Enable HTTPS")
+var (
+	cfg  = GetDefaultConfig()
+	once sync.Once
+)
 
-	// file-config
-	flag.StringVar(&cfgFilePath, "config", "", "Config file path")
-	flag.StringVar(&cfgFilePath, "c", "", "Config file path")
+// GetConfig gets new config from flags, env or file.
+func GetConfig() Config {
+	once.Do(func() {
+		var (
+			fileCfg, envCfg, flagCfg Config
+			cfgFilePath              string
+		)
 
-	flag.Parse()
+		flag.StringVar(&flagCfg.ServerAddress, "a", "", "Server address")
+		flag.StringVar(&flagCfg.BaseURL, "b", "", "Base URL")
+		flag.StringVar(&flagCfg.StoragePath, "f", "", "Storage path")
+		flag.StringVar(&flagCfg.BasePath, "d", "", "DataBase path")
+		flag.BoolVar(&flagCfg.EnableHTTPS, "s", false, "Enable HTTPS")
 
-	if cfgFilePath != "" {
-		file, err := os.ReadFile(cfgFilePath)
-		if err != nil {
-			log.Fatalln("Failed parse config file:", err)
+		flag.StringVar(&cfgFilePath, "c", "", "Config file path")
+		flag.StringVar(&cfgFilePath, "config", "", "Config file path")
+
+		flag.Parse()
+
+		if cfgFilePath != "" {
+			file, err := os.ReadFile(cfgFilePath)
+			if err != nil {
+				log.Fatalln("Failed parse config file:", err)
+			}
+
+			if err = json.Unmarshal(file, &fileCfg); err != nil {
+				log.Fatalln("Failed unmarshal config file:", err)
+			}
 		}
-
-		err = json.Unmarshal(file, &fileCfg)
+		// env config.
+		err := env.Parse(&envCfg)
 		if err != nil {
-			log.Fatalln("Failed unmarshal config file:", err)
+			log.Fatalln("Failed parse config:", err)
 		}
-	}
+		// change config by priority.
+		cfg.ChangeByPriority(fileCfg)
+		cfg.ChangeByPriority(envCfg)
+		cfg.ChangeByPriority(flagCfg)
+	})
 
-	// env-config
-	if err := env.Parse(&envCfg); err != nil {
-		log.Fatalln("Failed parse config: ", err)
-	}
-
-	// change config by priority.
-	cfg.ChangeByPriority(fileCfg)
-	cfg.ChangeByPriority(envCfg)
-	cfg.ChangeByPriority(flagCfg)
+	return cfg
 }
 
 // ChangeByPriority changes config by priority.
-func (cfg *Config) ChangeByPriority(choiceCfg Config) {
-	values := reflect.ValueOf(choiceCfg)
-	oldValues := reflect.ValueOf(&cfg).Elem()
+func (c *Config) ChangeByPriority(newCfg Config) {
+	values := reflect.ValueOf(newCfg)
+	oldValues := reflect.ValueOf(c).Elem()
 
 	for j := 0; j < values.NumField(); j++ {
 		if !values.Field(j).IsZero() {
