9777525d6440c67aa6011ed6306f8ccd88831679
foagdude <nk-again@rambler.ru>
2023-04-02 18:28:15 +0700

    + test

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,6 +12,11 @@ import (
 	"github.com/caarlos0/env/v7"
 )
 
+var (
+	cfg  = GetDefaultConfig()
+	once sync.Once
+)
+
 // Config Application config.
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" json:"server_address,omitempty"`
@@ -22,27 +27,17 @@ type Config struct {
 	EnableHTTPS     bool `env:"ENABLE_HTTPS" json:"enable_https,omitempty"`
 }
 
-// GetDefaultConfig gets default config.
-func GetDefaultConfig() Config {
-	return Config{
-		ServerAddress:   ":8080",
-		BaseURL:         "http://127.0.0.1:8080",
-		DBMigrationPath: "file://migrations",
-		EnableHTTPS:     false,
+// ChangeByPriority changes config by priority.
+func (c *Config) ChangeByPriority(newCfg Config) {
+	values := reflect.ValueOf(newCfg)
+	oldValues := reflect.ValueOf(c).Elem()
+
+	for j := 0; j < values.NumField(); j++ {
+		if !values.Field(j).IsZero() {
+			oldValues.Field(j).Set(values.Field(j))
+		}
 	}
 }
-
-// GetBenchConfig gets config for benchmarks.
-func GetBenchConfig() Config {
-	return Config{
-		DBMigrationPath: "file://../../migrations",
-	}
-}
-
-var (
-	cfg  = GetDefaultConfig()
-	once sync.Once
-)
 
 // GetConfig gets new config from flags, env or file.
 func GetConfig() Config {
@@ -87,14 +82,30 @@ func GetConfig() Config {
 	return cfg
 }
 
-// ChangeByPriority changes config by priority.
-func (c *Config) ChangeByPriority(newCfg Config) {
-	values := reflect.ValueOf(newCfg)
-	oldValues := reflect.ValueOf(c).Elem()
+// GetDefaultConfig gets default config.
+func GetDefaultConfig() Config {
+	return Config{
+		ServerAddress:   ":8080",
+		BaseURL:         "http://127.0.0.1:8080",
+		DBMigrationPath: "file://migrations",
+		EnableHTTPS:     false,
+	}
+}
 
-	for j := 0; j < values.NumField(); j++ {
-		if !values.Field(j).IsZero() {
-			oldValues.Field(j).Set(values.Field(j))
-		}
+// GetBenchConfig gets config for benchmarks.
+func GetBenchConfig() Config {
+	return Config{
+		DBMigrationPath: "file://../../migrations",
+	}
+}
+
+// GetTestConfig gets config for tests.
+func GetTestConfig() Config {
+	return Config{
+		ServerAddress: ":8080",
+		BaseURL:       "http://127.0.0.1:8081",
+		StoragePath:   "file_storage.txt",
+		BasePath:      "mockedDB",
+		EnableHTTPS:   false,
 	}
 }
