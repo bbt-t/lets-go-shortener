495835a7def4667dce8ef0afd8b5d8945dec4fb7
foagdude <nk-again@rambler.ru>
2023-04-13 11:45:12 -0300

    test

--- internal/usecase/shortener.go
+++ internal/usecase/shortener.go
@@ -30,18 +30,22 @@ func NewShortenerService(cfg config.Config, s Repository) *ShortenerService {
 	}
 }
 
+// CreateShort creates short url from original.
 func (s ShortenerService) CreateShort(userID string, urls ...string) ([]string, error) {
 	return s.storage.CreateShort(userID, urls...)
 }
 
+// GetOriginal gets original url from short.
 func (s ShortenerService) GetOriginal(id string) (string, error) {
 	return s.storage.GetOriginal(id)
 }
 
+// MarkAsDeleted deletes url.
 func (s ShortenerService) MarkAsDeleted(userID string, ids ...string) error {
 	return s.storage.MarkAsDeleted(userID, ids...)
 }
 
+// GetURLArrayByUser gets all urls.
 func (s ShortenerService) GetURLArrayByUser(userID string) ([]entity.URLs, error) {
 	return s.storage.GetURLArrayByUser(userID)
 }
@@ -50,10 +54,12 @@ func (s ShortenerService) PingDB() error {
 	return s.storage.PingDB()
 }
 
+// GetConfig for get cfg.
 func (s ShortenerService) GetConfig() config.Config {
 	return s.storage.GetConfig()
 }
 
+// GetStatistic gets total count of users and urls.
 func (s ShortenerService) GetStatistic() (entity.Statistic, error) {
 	return s.storage.GetStatistic()
 }
