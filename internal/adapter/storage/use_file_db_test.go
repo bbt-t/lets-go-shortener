6014e655e4a568e95effe2792ff68f5b1e90cb88
foagdude <nk-again@rambler.ru>
2023-04-13 18:18:19 -0300

    test

--- internal/adapter/storage/use_file_db_test.go
+++ internal/adapter/storage/use_file_db_test.go
@@ -1,7 +1,6 @@
 package storage
 
 import (
-	"github.com/bbt-t/lets-go-shortener/internal/entity"
 	"testing"
 
 	"github.com/bbt-t/lets-go-shortener/internal/config"
@@ -10,7 +9,7 @@ import (
 
 func TestFileStorage_GetConfig(t *testing.T) {
 	cfg := config.GetTestConfig()
-	s, err := NewStorage(cfg)
+	s, err := newFileStorage(cfg)
 
 	assert.NoError(t, err)
 	assert.Equal(t, cfg, s.GetConfig())
@@ -18,7 +17,7 @@ func TestFileStorage_GetConfig(t *testing.T) {
 }
 
 func TestFileStorage_PingDB(t *testing.T) {
-	s, err := NewStorage(config.GetTestConfig())
+	s, err := newFileStorage(config.GetTestConfig())
 
 	assert.NoError(t, err)
 	assert.NoError(t, s.PingDB())
@@ -33,30 +32,5 @@ func TestFileStorage_MarkAsDeleted(t *testing.T) {
 	err = s.MarkAsDeleted("user12", "1234") // do nothing.
 
 	assert.NoError(t, err)
-	assert.NoError(t, err)
-}
-
-func TestFileStorage_GetStatistic(t *testing.T) {
-	cfg := config.GetTestConfig()
-
-	s, err := newFileStorage(cfg)
-	assert.NoError(t, err)
-
-	stat, err := s.GetStatistic()
-	assert.NoError(t, err)
-	assert.Equal(t, entity.Statistic{
-		Urls:  0,
-		Users: 0,
-	}, stat)
-
-	_, err = s.CreateShort("user12", "https:/123.ru")
-	assert.NoError(t, err)
-
-	stat, err = s.GetStatistic()
-	assert.NoError(t, err)
-	assert.Equal(t, entity.Statistic{
-		Urls:  1,
-		Users: 0,
-	}, stat)
 	assert.NoError(t, err)
 }
